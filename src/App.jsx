58b5cbd300c6f27d457819f6177a86ae3feb1674
Deborah Hasanago <deborahhasanago30@gmail.com>
2024-10-13 11:00:07 +0200

    API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import FetchMovie from "./FetchMovie";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return {};
+  return (
+    <div>
+      <FetchMovie />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/FetchMovie.jsx
@@ -0,0 +1,36 @@
+// A React hook/tool that tells a component to do something = UseEffect e.g fetch data in this case.
+// A hook/storage box used to store information and updated when needed = UseState.
+import React, { useEffect, useState } from "react";
+
+// the function that will fetch the movies and display it.
+function FetchMovie() {
+  // movieList = an empty storage box where we are going to add the movies that we will retrive.
+  //setMovieList = the tool that is going to grab the movies and add it into the movieList.
+  const [movieList, setMovieList] = useState([]);
+
+  // reminds React to fetch the movies when the component is shown.
+  useEffect(() => {
+    //This makes an HTTP GET request to The Movie Database API to fetch a list of "now playing movies".
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=1d1d8844ae1e746c459e7be85c15c840"
+    )
+      //converts the API response into JSON format in order for react to read it.
+      .then((response) => response.json())
+      // passes the results (array of movies) to setMovieList in order to update the component state or movieList
+      .then((json) => setMovieList(json.results))
+      // incase errors occur
+      .catch((err) => console.log(err));
+  }, []);
+
+  // loop (map) inside the "movieList" array and display each "movie" e.g title
+  return (
+    <div>
+      {movieList.map((movie) => (
+        <h1>{movie.title}</h1>
+      ))}
+    </div>
+  );
+}
+
+// export the component so it can be used in other parts of the application
+export default FetchMovie;
